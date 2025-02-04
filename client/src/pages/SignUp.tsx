bd090b6c4d30c154cd92fcd03431981e7c35ed6c
caszofficial <santiagozapata07@gmail.com>
2025-02-04 17:53:54 -0500

    signup form ready

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,23 +1,36 @@
-import { Box, Button } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from "@mui/material";
 
 import axios from "axios";
 import { Field, Form, Formik } from "formik";
 import { useEffect, useState } from "react";
+import * as Yup from "yup";
 
-const fieldStyle = {
-  width: "80%",
-  margin: "0px 10px",
-  fontSize: "20px",
-  padding: "10px 20px",
-  border: "1px solid gray",
-  borderRadius: "5px",
-  outline: "none",
-};
+import "./signup.css";
 
 interface Services {
   id: string;
   name: string;
 }
+const signUpSchema = Yup.object().shape({
+  name: Yup.string().required("Name is a Required Field"),
+  email: Yup.string()
+    .email("Invalid Email")
+    .required("Email is a Required Field"),
+  password: Yup.string()
+    .min(8, "Password bust be 8+ characters long")
+    .required("Must be at Least 8 Characters"),
+  address: Yup.string().required("Address is a Required Field"),
+  city: Yup.string().required("City is a Required Field"),
+  zipCode: Yup.number().required("Zip Code is a Required Field"),
+});
 
 const SignUp = () => {
   const [services, setServices] = useState<Services[]>([]);
@@ -32,37 +45,36 @@ const SignUp = () => {
   }, []);
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        margin: "auto",
-        minHeight: "100vh",
-      }}
-    >
-      <Box
-        sx={{
-          width: "100%",
-          margin: "50px",
-          padding: "50px",
-          display: "flex",
-          justifyContent: "space-evenly",
-          alignItems: "center",
-        }}
-      >
-        <Box>
-          <img
-            src="https://firebasestorage.googleapis.com/v0/b/sds-main-29a46.firebasestorage.app/o/images%2Flogo.png?alt=media&token=6defccae-3a0c-4333-80a8-1c1ef024c917"
-            alt=""
-          />
+    <Box className="signup-container">
+      <Box className="logo-container">
+        <img
+          className="logo"
+          src="https://firebasestorage.googleapis.com/v0/b/sds-main-29a46.firebasestorage.app/o/images%2Flogo.png?alt=media&token=6defccae-3a0c-4333-80a8-1c1ef024c917"
+          alt=""
+        />
+      </Box>
+
+      <Box className="main-content-container">
+        <Box className="main-container1">
+          <Typography
+            className="container1-title"
+            variant="h1"
+            sx={{
+              textAlign: "center",
+              textWrap: "balance",
+              fontSize: "40px",
+              color: "#000",
+            }}
+          >
+            You're one step away from the
+            <span> lead generation </span>
+            future.
+          </Typography>
         </Box>
-        <Box
-          sx={{
-            backgroundColor: "#f1f1f1",
-            padding: "40px 30px",
-            borderRadius: "10px",
-          }}
-        >
+        <Box className="main-container2">
           <Formik
+            validationSchema={signUpSchema}
+            validateOnChange
             initialValues={{
               name: "",
               email: "",
@@ -95,73 +107,148 @@ const SignUp = () => {
               }
             }}
           >
-            {({ values, setFieldValue }) => {
+            {({ values, setFieldValue, isValid, errors, touched, dirty }) => {
               // Usamos useEffect para ver los valores cada vez que cambian
               useEffect(() => {
                 console.log("Selected services:", values.services);
               }, [values.services]);
 
               return (
-                <Form
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    gap: "20px",
-                  }}
-                >
-                  <Field name="name" style={fieldStyle} placeholder="Name" />
-                  <Field name="email" style={fieldStyle} placeholder="Email" />
+                <Form className="signup-form">
+                  <Typography
+                    variant="h4"
+                    sx={{ fontWeight: "bold", color: "#1d960d" }}
+                  >
+                    Sign Up
+                  </Typography>
+                  <Field name="name" placeholder="Name" />
+
+                  {errors.name && touched.name ? (
+                    <div
+                      style={{
+                        color: "#f16464",
+                        fontSize: "12px",
+                        fontWeight: "bold",
+                        fontFamily: "Sans-serif",
+                      }}
+                    >
+                      {errors.name}
+                    </div>
+                  ) : null}
+
+                  <Field name="email" placeholder="Email" />
+                  {errors.email && touched.email ? (
+                    <div
+                      style={{
+                        color: "#f16464",
+                        fontSize: "12px",
+                        fontWeight: "bold",
+                        fontFamily: "Sans-serif",
+                      }}
+                    >
+                      {errors.email}
+                    </div>
+                  ) : null}
                   <Field
                     name="password"
-                    style={fieldStyle}
                     placeholder="Password"
                     type="password"
                   />
-                  <Field name="city" style={fieldStyle} placeholder="City" />
-                  <Field
-                    name="address"
-                    style={fieldStyle}
-                    placeholder="Address"
-                  />
-                  <Field
-                    name="zipCode"
-                    style={fieldStyle}
-                    placeholder="Zip Code"
-                  />
+                  {errors.password && touched.password ? (
+                    <div
+                      style={{
+                        color: "#f16464",
+                        fontSize: "12px",
+                        fontWeight: "bold",
+                        fontFamily: "Sans-serif",
+                      }}
+                    >
+                      {errors.password}
+                    </div>
+                  ) : null}
+                  <Field name="address" placeholder="Address" />
+                  {errors.address && touched.address ? (
+                    <div
+                      style={{
+                        color: "#f16464",
+                        fontSize: "12px",
+                        fontWeight: "bold",
+                        fontFamily: "Sans-serif",
+                      }}
+                    >
+                      {errors.address}
+                    </div>
+                  ) : null}
+                  <Field name="city" placeholder="City" />
+                  {errors.city && touched.city ? (
+                    <div
+                      style={{
+                        color: "#f16464",
+                        fontSize: "12px",
+                        fontWeight: "bold",
+                        fontFamily: "Sans-serif",
+                      }}
+                    >
+                      {errors.city}
+                    </div>
+                  ) : null}
+                  <Field name="zipCode" placeholder="Zip Code" required />
 
-                  {/* Campo select múltiple para servicios */}
-                  <Field
-                    name="services"
-                    as="select"
-                    multiple
-                    style={fieldStyle}
-                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-                      const selectedOptions = Array.from(
-                        e.target.selectedOptions,
-                        (option: HTMLOptionElement) => option.value
-                      );
-                      // Convertimos las opciones seleccionadas a números
-                      const newServices = selectedOptions.map(Number);
+                  {errors.zipCode && touched.zipCode ? (
+                    <div
+                      style={{
+                        color: "#f16464",
+                        fontSize: "12px",
+                        fontWeight: "bold",
+                        fontFamily: "Sans-serif",
+                      }}
+                    >
+                      {errors.zipCode}
+                    </div>
+                  ) : null}
 
-                      // Concatenamos los nuevos valores al arreglo existente de services
-                      const updatedServices = [
-                        ...values.services,
-                        ...newServices,
-                      ];
-                      setFieldValue("services", updatedServices); // Actualizamos el valor en Formik
-                    }}
+                  <Typography sx={{ margin: "20px 0" }}>
+                    Please select the services you offer in the dropdown below
+                  </Typography>
+                  <FormControl
+                    variant="outlined"
+                    size="small"
+                    className="signup-select"
                   >
-                    <option value="">Select services</option>
-                    {services.map((service) => (
-                      <option key={service.id} value={service.id}>
-                        {service.name}
-                      </option>
-                    ))}
-                  </Field>
-
-                  <Button type="submit" fullWidth>
+                    <InputLabel id="services-label">Select Services</InputLabel>
+                    <Select
+                      labelId="services-label"
+                      name="services"
+                      multiple
+                      value={values.services}
+                      onChange={(e) => {
+                        const selectedValues = e.target.value;
+                        setFieldValue("services", selectedValues);
+                      }}
+                      renderValue={(selected) =>
+                        selected
+                          .map(
+                            (id) =>
+                              services.find((service) => service.id === id)
+                                ?.name
+                          )
+                          .join(", ")
+                      }
+                      label="Select Services"
+                      style={{ backgroundColor: "white" }}
+                    >
+                      {services.map((service) => (
+                        <MenuItem key={service.id} value={service.id}>
+                          {service.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <Button
+                    type="submit"
+                    className="signup-submit"
+                    disabled={!isValid || !dirty}
+                  >
                     Submit
                   </Button>
                 </Form>
