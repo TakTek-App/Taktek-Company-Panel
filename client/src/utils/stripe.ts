675cda787a32a303fd3fbd4cf21608b0f887e7ac
Santiago <santiagozapata07@gmail.com>
2025-02-11 23:19:59 -0500

    fixed stripe config file

--- client/src/utils/stripe.ts
+++ client/src/utils/stripe.ts
@@ -1,5 +1,5 @@
 import { loadStripe, Stripe } from "@stripe/stripe-js";
 
-const stripePromise: Promise<Stripe | null> = loadStripe("pk_test_51Qn2WiQ61pydXpHxZ9hkjpAXtvwQ8207k0rQnN9EM09ENCVjDcjtyaPzchs1piIO6ueIx6wfqeMY0lYcEPLzuBvy00X4cdEEDo");
+const stripePromise: Promise<Stripe | null> = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
 export default stripePromise;
